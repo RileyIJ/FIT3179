28e572588a3a307b43051a6620890c6e5e3b01c5
RileyIJ <90737559+RileyIJ@users.noreply.github.com>
2023-10-15 23:15:01 +1100

    Add files via upload

---
+++ js/embed_script.js
@@ -0,0 +1,54 @@
+// World Export map
+var spec2 = "js/world_export_map.vg.json"
+
+vegaEmbed('#world_export_map', spec2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+// GDP donut chart
+var spec2 = "js/GDP_percentage_donut_chart.vg.json"
+
+vegaEmbed('#GDP_percentage_donut_chart', spec2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+// Employment Waffle Chart
+var spec2 = "js/ag_employment_waffle.vg.json"
+
+vegaEmbed('#ag_employment_waffle', spec2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+// Water Use donut chart
+var spec2 = "js/water_use_donut_chart.vg.json"
+
+vegaEmbed('#water_use_donut_chart', spec2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+// Diesel use dot plot
+var spec2 = "js/diesel_use_dot_plot.vg.json"
+
+vegaEmbed('#diesel_use_dot_plot', spec2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+// Animal slaughter Figures projection line chart
+var spec2 = "js/animal_slaughter_figures_projection.vg.json"
+
+vegaEmbed('#animal_slaughter_figures_projection', spec2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+// Meat Produciton vs year histogram
+var spec2 = "js/meat_production_year.vg.json"
+
+vegaEmbed('#meat_production_year', spec2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
